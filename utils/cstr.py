8ec2ab27000130d77d13cc6ebebd97d01508afac
charliejuc <charliejuc@gmail.com>
2019-03-12 19:35:06 +0100

    Fixed bug in 'str_equals_to_dict' when string contains more than one '='.

--- utils/cstr.py
+++ utils/cstr.py
@@ -9,6 +9,6 @@ def str_to_raw(string):
 
 def str_equals_to_dict(env):
 	equal = '='
-	env_split = ( e.split(equal) for e in env )
+	env_split = ( e.split(equal, 1) for e in env )
 
 	return { key: value for key, value in env_split }
